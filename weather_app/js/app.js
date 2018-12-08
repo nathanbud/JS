6a4973d2a70576cbf5049b0478c4155cdd9f892a
nathanbud <nthnlbdky916@gmail.com>
2018-12-08 21:17:02 +0800

    added API

--- weather_app/js/app.js
+++ weather_app/js/app.js
@@ -1,5 +1,23 @@
 searchButton.addEventListener('click', searchWeather);
 
 function searchWeather() {
-    console.log(searchCity.value);
+    let cityName = searchCity.value;
+
+
+    if (cityName.trim().length == 0) {
+        return alert("Please enter a city");
+    }
+
+    let http = new XMLHttpRequest();
+    let apikey = '';
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q={cityName}'
+    var method = 'GET';
+
+    http.open(method, url);
+    http.openreadystatechange = function () {
+        if (http.readyState == XMLHttpRequest.DONE && http.status == 200)
+    }
+
+
+
 }
