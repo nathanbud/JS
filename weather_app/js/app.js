71e8ccdec5b04cc0e6ab2e7b41c8c9ff279e7787
nathanbud <nthnlbdky916@gmail.com>
2019-01-10 11:04:42 +0800

    added weather constructor

--- weather_app/js/app.js
+++ weather_app/js/app.js
@@ -9,18 +9,23 @@ function searchWeather() {
     }
 
     let http = new XMLHttpRequest();
-    let apikey = '';
-    let url = 'http://api.openweathermap.org/data/2.5/weather?q={cityName}'
+    let apiKey = 'e72a4b1901da3ae63f2b229a8878f024';
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName} &units=metric&appid=${apiKey}`;
     var method = 'GET';
 
     http.open(method, url);
-    http.openreadystatechange = function () {
+    http.onreadystatechange = function () {
         if (http.readyState == XMLHttpRequest.DONE && http.status == 200) {
             let data = JSON.parse(http.responseText);
+            console.log("weatherData");
+            let weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            weatherData.temperature = data.main.temp;
+            console.log(weatherData);
         } else if (http.readyState === XMLHttpRequest.DONE) {
             alert('Something went wrong!');
         }
-    }
+    };
+    http.send();
 
 
 
