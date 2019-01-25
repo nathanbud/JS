51e426d83c25c928911728a220bbf73f1a6eb565
nathanbud <nthnlbdky916@gmail.com>
2019-01-25 16:22:04 +0800

    add node and js

--- exercise/js/myjs.js
+++ exercise/js/myjs.js
@@ -1,9 +1,15 @@
+const fname = document.getElementById('fname');
+const lname = document.querySelector('#lname');
 const submit = document.querySelector('#submit');
 
 submit.addEventListener('click', function (e) {
-    const fname = document
-        .querySelector('#fname')
-        .value;
-    console.log(e.target);
-    alert(`First name: ${fname}`);
+    console.log(submit);
+    console.log('fname', fname.value);
+    console.log('lname', lname.value);
+    // const fname = document
+    //     .querySelector('#fname')
+    //     .value;
+    // console.log(e.target);
+    // alert(`First name: ${fname}`);
+    
 });
