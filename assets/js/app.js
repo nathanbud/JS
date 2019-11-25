96eccfc853717e30f837170ac893da7634c91c1c
nathanbud <nthnlbdky916@gmail.com>
2019-11-25 22:03:24 +0800

    started JS 30

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,21 @@
-
-$('.multi-level-dropdown .dropdown-submenu > a').on("mouseenter", function(e) {
-    var submenu = $(this);
-    $('.multi-level-dropdown .dropdown-submenu .dropdown-menu').removeClass('show');
-    submenu.next('.dropdown-menu').addClass('show');
-    e.stopPropagation();
+$(function() {
+    // ------------------------------------------------------- //
+    // Multi Level dropdowns
+    // ------------------------------------------------------ //
+    $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+  
+      $(this).siblings().toggleClass("show");
+  
+  
+      if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+      }
+      $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+        $('.dropdown-submenu .show').removeClass("show");
+      });
+  
+    });
   });
-
-  $('.multi-level-dropdown .dropdown').on("hidden.bs.dropdown", function() {
-    // hide any open menus when parent closes
-    $('.multi-level-dropdown .dropdown-menu.show').removeClass('show');
-  });
+  
