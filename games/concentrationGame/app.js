a7703cb7cf4ca982a3759022d5d74da357ef6dd7
nathanbud <nthnlbdky916@gmail.com>
2019-11-29 23:34:01 +0800

    Finished with the game

--- games/concentrationGame/app.js
+++ games/concentrationGame/app.js
@@ -1,12 +1,15 @@
 var resetButton = document.getElementById('reset-button');
 var gameSquares = [];
+var firstSquare = null;
 
 var colors = [];
 for (var i = 0; i < 10; i++){
     colors.push('square-'+ i);
 }
 
-function Gamesquare(el, color){
+
+
+function GameSquare(el, color){
     this.el = el;
     this.isOpen = false;
     this.isLocked = false;
@@ -14,24 +17,30 @@ function Gamesquare(el, color){
     this.setColor(color);
 }
 
-Gamesquare.prototype.handleEvent = function(e){
+GameSquare.prototype.handleEvent = function(e){
     switch (e.type){
         case "click":
-            if(this.isOpen || this.isLocked){
+            if(this.isOpen || this.isLocked ){
                 return;
             }
             this.isOpen = true;
             this.el.classList.add('flip');
+            checkGame(this);
     }
 }
 
-Gamesquare.prototype.reset = function(){
+GameSquare.prototype.reset = function(){
     this.isOpen = false;
     this.isLocked = false;
     this.el.classList.remove('flip');
 }
 
-Gamesquare.prototype.setColor = function(color){
+GameSquare.prototype.lock = function() {
+    this.isLocked = true;
+    this.isOpen = true;
+  }
+
+GameSquare.prototype.setColor = function(color){
     this.el.children[0].children[1].classList.remove(this.color);
     this.color = color;
     this.el.children[0].children[1].classList.add(color);
@@ -54,6 +63,11 @@ function getSomeColors() {
     return randomColors.concat(randomColors.slice());
     
   }
+ 
+
+
+
+  
 
 
 function setupGame(){
@@ -64,8 +78,51 @@ function setupGame(){
         var index = random(randomColors.length); 
         var color = randomColors.splice(index, 1)[0];
          // Use that color to initialize the GameSquare
-        gameSquares.push(new Gamesquare(array[i], color));
+        gameSquares.push(new GameSquare(array[i], color));
     }
 }
+
+function checkGame(gameSquare) {
+    if (firstSquare === null) {
+      firstSquare = gameSquare;
+      return
+    }
+  
+    if (firstSquare.color === gameSquare.color) {
+      firstSquare.lock();
+      gameSquare.lock();
+    } else {
+      var a = firstSquare;
+      var b = gameSquare;
+      setTimeout(function() {
+        a.reset();
+        b.reset();
+        firstSquare = null;
+      }, 400);
+    }
+    firstSquare = null;
+  }
+
+  function randomizeColors() {
+    var randomColors = getSomeColors();
+    gameSquares.forEach(function(gameSquare) {
+      var color = randomColors.splice(random(randomColors.length), 1)[0];
+      gameSquare.setColor(color);
+    });
+  }
+
+  function clearGame() {
+    gameSquares.forEach(function(gameSquare) {
+      gameSquare.reset();
+    });
+    setTimeout(function() {
+      randomizeColors();
+    }, 500);
+  }
+
+//   function reset(){
+//     clearGame();
+//   }
+//   resetButton.addEventListener('click',reset);
 
 setupGame();
